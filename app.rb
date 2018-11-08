7d2a38b53ac7475b0d1152417812aa8d64ee78c6
transfer76 <shina76@ukr.net>
2018-11-08 23:57:21 +0200

    add barbers migration

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ set :database,"sqlite3:barbershop.db"
 class Client < ActiveRecord::Base
 end
 
+class Barber < ActiveRecord::Base
+end
+
+
 get '/' do
 	erb "Hello!"			
 end
--- db/migrate/20181108192001_create_clients.rb
+++ db/migrate/20181108192001_create_clients.rb
@@ -1,13 +1,13 @@
 class CreateClients < ActiveRecord::Migration[5.2]
   def change
-  	creatd_table :client do |t|
+  	create_table :client do |t|
   		t.text :name
   		t.text :phone
   		t.text :datestamp
   		t.text :barber
   		t.text :color
 
-  		t.timestamp
+  		t.timestamps
   	end	
   end
 end
