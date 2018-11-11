3b73bf3ca5b1b99d999476f1fd0cbb8bf9fbdf18
transfer76 <shina76@ukr.net>
2018-11-11 14:48:21 +0200

    display error text

--- app.rb
+++ app.rb
@@ -34,7 +34,8 @@ post '/visit' do
 	  if c.save
 	     erb "<h2>Спасибо, вы записались!</h2>"
 	   else
-	      erb "<h2>Error</h2>"  
+	   	  @error=c.errors.full_messages.first
+	      erb :visit 
 	   end   
 
 end
