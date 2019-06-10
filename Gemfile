76a35d32a76db052439f778acfc5822f75565f10
transfer76 <shina76@ukr.net>
2019-06-11 01:07:56 +0300

    Check codestyle

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "sinatra"
-gem "sqlite3", '~> 1.3.6'
-gem "activerecord"
-gem "sinatra-activerecord"
-gem "sinatra-contrib"
+gem 'activerecord'
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'sinatra-contrib'
+gem 'sqlite3', '~> 1.3.6'
 
 group :development do
-	    gem "tux"
+  gem 'tux'
 end
--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
+set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true, length: { minimum: 3 }
-	validates :phone, presence: true
-	validates :datestamp, presence: true
-	validates :color, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :phone, presence: true
+  validates :datestamp, presence: true
+  validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -32,7 +31,7 @@ end
 post '/visit' do
   @c = Client.new params[:client]
   if @c.save
-    erb "<h2>Спасибо, вы записались!</h2>"
+    erb '<h2>Спасибо, вы записались!</h2>'
   else
     @error = @c.errors.full_messages.first
     erb :visit
